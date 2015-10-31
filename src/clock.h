ef06a3a894682f2f9fd257892fea7035ddfa1e79
Andrés Mata <andreseduardomp@gmail.com>
2015-10-31 21:09:39 +0000

    Iniciado el projecto con PlatformIO para poder subirlo a la placa Arduino Uno desde la terminal

---
+++ src/clock.h
@@ -0,0 +1,89 @@
+const String MONTH_NAME[12] = {"Ene", "Feb", "Mar", "Abr", "May", "Jun", "Jul", "Ago", "Sept", "Oct", "Nov", "Dic"};
+const int POMODORO_MIN = 25;
+const int HOUR_LIMIT = 23;
+const int MIN_LIMIT = 59;
+const int SEC_LIMIT = 59;
+const int INTERVAL = 1000;
+int hour = 0;
+int min = 0;
+int sec = 0;
+int day = 0;
+int month = 0;
+int year = 0;
+int pomodoroMinLeft = POMODORO_MIN;
+int pomodoroSecLeft = 0;
+boolean pomodoro = false;
+
+unsigned long previousMillis = 0;
+
+void addSecond() {
+  if(sec < SEC_LIMIT) {
+            sec++;
+        } else {
+            sec = 0;
+            if(min < MIN_LIMIT) {
+                min++;
+            } else {
+                min = 0;
+                if(hour < HOUR_LIMIT) {
+                    hour++;
+                } else {
+                    hour = 0;
+                }
+            }
+        }
+}
+
+void updateTime(int _hour, int _min, int _sec, int _day, int _month, int _year) {
+  lcd.clear();
+  initLCD();
+  hour = _hour;
+  min = _min;
+  sec = _sec;
+  day = _day;
+  month = _month;
+  year = _year;
+  int dayLen = calculateDigits((double) day);
+  lcd.setCursor(0,0);
+  lcd.print(day);
+  lcd.setCursor(1 + dayLen,0);
+  lcd.print(MONTH_NAME[month]);
+}
+
+void changePomodoroStatus() {
+  if(pomodoro) {
+    pomodoro = false;
+  } else {
+    pomodoro = true;
+    pomodoroMinLeft = POMODORO_MIN;
+    pomodoroSecLeft = 0;
+  }
+}
+
+void addPomodoroSecond() {
+  if(pomodoroSecLeft <= 0 && pomodoroMinLeft <= 0) {
+    pomodoro = false;
+  } else if(pomodoroSecLeft <= 0) {
+    pomodoroSecLeft = 59;
+    pomodoroMinLeft--;
+  } else {
+    pomodoroSecLeft--;
+  }
+}
+
+void clockContinue() {
+  unsigned long currentMillis = millis();
+  if(currentMillis - previousMillis >= INTERVAL) {
+    //Si entra aquí es que ha pasado un segundo
+    previousMillis = currentMillis;
+    addSecond();
+    if(pomodoro) {
+      addPomodoroSecond();
+      int digits[4] = {pomodoroMinLeft / 10, pomodoroMinLeft % 10, pomodoroSecLeft / 10, pomodoroSecLeft % 10};
+      updateDigits(digits);
+    } else {
+      int digits[4] = {hour / 10, hour % 10, min / 10, min % 10};
+      updateDigits(digits);
+    }
+  }
+}
---
+++ src/projectClockV2.ino
@@ -0,0 +1,76 @@
+#include "lib/Wire.h"
+#include "lib/LiquidCrystal_I2C.h"
+LiquidCrystal_I2C  lcd(0x27,2,1,0,4,5,6,7,3, POSITIVE);
+#include "screen.h"
+#include "clock.h"
+const int MIN_RAW_DATA_LENGTH = 30;
+const int MAX_RAW_DATA_LENGTH = 39;
+char raw[MAX_RAW_DATA_LENGTH] = {};
+double values[12] = {};
+
+//22:59:41:27:10:2015:0:10.4:98:24.0:39:0-
+
+void setup() {
+    Serial.begin(9600);
+    initialize();
+}
+
+void loop() {
+  memset(&values, NULL, sizeof(values));
+  if(Serial.available() > 0) {
+    byte readedBytes = Serial.readBytesUntil('-' ,raw, MAX_RAW_DATA_LENGTH);
+    if(readedBytes >= MIN_RAW_DATA_LENGTH && readedBytes <= MAX_RAW_DATA_LENGTH) {
+      //Si entra aquí es que ha leido correctamente los bytes
+      //Serial.print("Se han leido : ");
+      //Serial.println(readedBytes, DEC);
+      //Hacemos un split en el char[] y guardamos los valores
+      char *pointer = strtok(raw, ":");
+      int counter = 0;
+      while(pointer != NULL && counter <= sizeof(values)) {
+        values[counter] = strtod(pointer, NULL);
+        //Serial.print("Introduciendo el valor en el array: ");
+        //Serial.println(values[counter]);
+        pointer = strtok(NULL, ":");
+        counter++;
+      }
+      //Comprobamos si ha habido errores y actualizamos los datos en pantalla.
+      if(values[6] != 1) {
+        updateValues();
+      } else {
+        printSync(true);
+        Serial.print("Se ha producido un error al recibir los datos: ");
+        Serial.println(values[6]);
+      }
+    }
+    memset(&raw, NULL, sizeof(raw));
+  }
+  clockContinue();
+  draw();
+}
+
+void updateValues() {
+  int hour = values[0];
+  int min = values[1];
+  int sec = values[2];
+  int day = values[3];
+  int month = values[4];
+  int year = values[5];
+  double temp = values[7];
+  int hum = values[8];
+  double localTemp = values[9];
+  int localHum = values[10];
+  int operation = values[11];
+  switch(operation) {
+    case 1:
+        changeBacklightState();
+    break;
+    case 2:
+        changePomodoroStatus();
+    break;
+    default:
+        updateTime(hour, min, sec, day, month, year);
+        updateWeather(temp, hum, localTemp, localHum);
+        printSync(false);
+    break;
+  }
+}
